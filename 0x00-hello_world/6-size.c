1bcb4453daa094d8ca7505e6ba50a2a4ae83ee66
Griff20 <griffinlevi18@gmail.com>
2022-09-08 17:19:05 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
-*
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ *
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 
@@ -20,5 +20,6 @@ int main(void)
 		printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 
 		printf("Size of a float: %d byte(s)\n", sizeof(float));
+		
 		return (0);
 }

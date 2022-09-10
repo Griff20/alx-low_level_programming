850f7700db17edf008674dc9036ed2a6c4ba3296
Griff20 <100014615+Griff20@users.noreply.github.com>
2022-09-10 22:48:08 +0300

    9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,31 @@
-iodg
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* betty style to function main goes there */
+/**
+ * main - Entry
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int c;
+	char k;
+
+	c = 48;
+	while  (i < 58)
+	{
+		putchar(c);
+		c++;
+	}
+
+	k = 'a';
+	while (k <= 'f')
+	{
+		putchar(k);
+		k++;
+	}
+	putchar('\n');
+
+	return (0);
+}

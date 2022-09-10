1b75252f3f2ebdb3b758b49d47eb3b5ef818c223
Griff20 <100014615+Griff20@users.noreply.github.com>
2022-09-10 21:56:19 +0300

    2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,19 @@
-hdfnfdaihf
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Return: Always void (Success)
+ */
+int main(void)
+{
+	char l;
+
+	l = 'a';
+	while (l <= 'z')
+	{
+		putchar(l);
+		l++;
+	}
+	putchar('\n');
+	return (0);
+}

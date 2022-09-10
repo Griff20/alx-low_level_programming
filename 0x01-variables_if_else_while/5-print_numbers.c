d10ccaaf3f5d66ac652bc3f7b41e95060e656265
Griff20 <100014615+Griff20@users.noreply.github.com>
2022-09-10 22:20:03 +0300

    5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,14 @@
-asfhwafhu
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* betty style to function main goes there */
+/**
+ * main - Entry
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	printf("0123456789\n");
+	return (0);
+}

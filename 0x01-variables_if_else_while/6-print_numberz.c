57aae9da6eba689428a50b34772ec430218acf2c
Griff20 <100014615+Griff20@users.noreply.github.com>
2022-09-10 22:27:35 +0300

    6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,23 @@
-nioohfddq
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* betty style to function main goes there */
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 48;
+	while  (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}

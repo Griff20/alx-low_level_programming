6c0b5c27b108229f34a7be91a074111e41a50abe
Griff20 <griffinlevi18@gmail.com>
2022-09-13 12:14:42 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,8 +8,13 @@
 
 int main(void)
 {
-	mxw = "_putchar";
-
-	printf("%s",mxw);
+	char x[9] = "_putchar";
+	int a=0;
+	while (a<8)
+	{
+		_putchar(x[a]);
+		a++;
+	}
+	_putchar('\n');
 	return (0);
 }

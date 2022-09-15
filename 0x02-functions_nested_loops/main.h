db83ac775eef712284d0ef7b9d6be7bfc20807e6
Griff20 <100014615+Griff20@users.noreply.github.com>
2022-09-15 12:15:02 +0300

    0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,25 @@
-#include "stdio.h"
+int _putchar(char x);
 
-/**
- * main - Entry point
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_alphabet();
-    return (0);
-}
+void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
+int _islower(int x);
+
+int _isalpha(int x);
+
+int print_sign(int n);
+
+int _abs(int n);
+
+int print_last_digit(int n);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int a, int b);
+
+void print_to_98(int n);
+
+void print_times_table(int n);

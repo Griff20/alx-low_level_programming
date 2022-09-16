64ba9a00c2700d9c1c813dc3aeb8eaa25a02b955
Griff20 <100014615+Griff20@users.noreply.github.com>
2022-09-16 12:30:43 +0300

    0

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main"
+/**
+ * main - prints alphabets
+ *
+ * Return: Success(0)
+ */
+void print_alphabet(void)
+{
+          char albet = 'a';
+          
+          while (albet <= 'z')
+          {
+              _putchar(albet);
+              albet++;
+          }
+          _putchar("\n");
+}
+	
+     

a0b80edf6f63ceaf9647ff837ab86f36c2bb54bb
Griff20 <griffinlevi18@gmail.com>
2022-09-20 12:53:05 -0700

    1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+/**
+ * swap_int - Replaces integer value with another
+ * @a - integer pointer
+ * @b - integer pointer
+ */
+void swap_int(int *a, int *b)
+{
+	int mw;
+
+	*b = mw;
+	*b = *a;
+	mw = *a;
+}

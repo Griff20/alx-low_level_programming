0486890035eb71192ef894874bac08c7a59ea842
Griff20 <griffinlevi18@gmail.com>
2022-09-20 15:16:19 -0700

    5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses given string
+ * @s: char string
+ */
+
+void rev_string(char *s)
+{
+	int m, x;
+	char mexico;
+
+	for (m = 0; s[m] != '\0';)
+	{
+		++x;
+	}
+	
+	for (x = 0; x < l / 2; ++x)
+	{
+		char = s[x];
+		s[x] = s[m - 1 - x];
+		s[m - 1 - x] = mexico;
+	}
+}

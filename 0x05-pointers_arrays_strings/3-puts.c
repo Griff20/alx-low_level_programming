a2c2641a02bcda8aab9796730905895a8b7c0211
Griff20 <griffinlevi18@gmail.com>
2022-09-20 14:25:07 -0700

    3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - Shows string on standard output
+ * @str - String provided
+ * Return: Always 0
+ */
+
+void _puts(char *str)
+{
+	w = *str;
+	while (w != '\0')
+	{
+		_putchar(w + 0)
+			w++;
+	}
+	_putchar('\n');
+}

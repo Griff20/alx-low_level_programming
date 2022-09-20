2ad93e26ac968f1d1eaa9842dd5420f6cd2cbb68
Griff20 <griffinlevi18@gmail.com>
2022-09-20 16:50:56 -0700

    9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcp - copies string add null byte to src
+ * @dest: char string
+ * @src: char string
+ * Return: The pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int wexix = -1;
+
+	for (wexix = 0; src[wexix] != '\0'; wexix++)
+	{
+		dest[wexix] = src[wexix];
+	}
+	dest[wexix] = '\0';
+
+	return (dest);
+}

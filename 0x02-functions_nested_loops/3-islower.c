58f506393835a211271f9343ef53a0f8fa87aa76
Griff20 <100014615+Griff20@users.noreply.github.com>
2022-09-21 03:31:40 +0300

    3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isower - checks and prints lower elements of string
+ * _islower - checks and prints lower elements of string
  * @c: char ctring
  * Return: 0 if succesful, 1 if it finds none
  */

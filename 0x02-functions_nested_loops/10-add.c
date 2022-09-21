72a74af4d5671c750ff5a583656520d2ce121921
Griff20 <griffinlevi18@gmail.com>
2022-09-20 19:08:56 -0700

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - aggregates two integers
+ * @a: integer
+ * @b: integer
+ * @xy: integer
+ * return: always 0
+ */
+
+int add(int a, int b)
+{
+	int a;
+	int b;
+	int xy;
+
+	xy = a + b;
+	return (xy);
+}

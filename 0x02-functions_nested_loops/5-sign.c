17171fd29bdd2d4e182c1ba08b835d529e0293fa
Griff20 <griffinlevi18@gmail.com>
2022-09-20 17:51:01 -0700

    5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - prints integers sign
+ * @n: integer
+ * Return: 1 if positive, 0 if zero and -1 if negative
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		return (0);
+	}
+}
